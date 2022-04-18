b9d5670ad2e8cdf160442c1642f1b8a2c88de595
andreespirela <andreespirela@outlook.com>
2022-04-18 11:21:19 -0400

    Smartweave initial

---
+++ src/contract-interact.ts
@@ -0,0 +1,353 @@
+import Arweave from 'arweave';
+import { CreateTransactionInterface } from 'arweave/node/common';
+import Transaction from 'arweave/node/lib/transaction';
+import { JWKInterface } from 'arweave/node/lib/wallet';
+import { loadContract } from './contract-load';
+import { readContract } from './contract-read';
+import { execute, ContractInteraction, ContractInteractionResult, ContractHandler } from './contract-step';
+import { evalSettings, unpackTags } from './utils';
+import { BlockData } from 'arweave/node/blocks';
+import SmartWeaveError, { SmartWeaveErrorType } from './errors';
+import { SmartWeaveGlobal } from './smartweave-global';
+
+/**
+ * Writes an interaction on the blockchain.
+ *
+ * This simply creates an interaction tx and posts it.
+ * It does not need to know the current state of the contract.
+ *
+ * @param arweave       an Arweave client instance
+ * @param wallet        a wallet private key
+ * @param contractId    the Transaction Id of the contract
+ * @param input         the interaction input, will be serialized as Json.
+ * @param tags          an array of tags with name/value as objects.
+ * @param target        if needed to send AR to an address, this is the target.
+ * @param winstonQty    amount of winston to send to the target, if needed.
+ * @param reward        custom reward for txs, if needed.
+ */
+export async function interactWrite(
+  arweave: Arweave,
+  wallet: JWKInterface | 'use_wallet',
+  contractId: string,
+  input: any,
+  tags: { name: string; value: string }[] = [],
+  target: string = '',
+  winstonQty: string = '',
+  reward?: string,
+): Promise<string> {
+  const interactionTx = await createTx(arweave, wallet, contractId, input, tags, target, winstonQty, reward);
+
+  const response = await arweave.transactions.post(interactionTx);
+
+  if (response.status !== 200) return null;
+
+  return interactionTx.id;
+}
+
+/**
+ * Simulates an interaction on the blockchain and returns the simulated transaction.
+ *
+ * This simply creates an interaction tx and posts it.
+ * It does not need to know the current state of the contract.
+ *
+ * @param arweave       an Arweave client instance
+ * @param wallet        a wallet private key
+ * @param contractId    the Transaction Id of the contract
+ * @param input         the interaction input, will be serialized as Json.
+ * @param tags          an array of tags with name/value as objects.
+ * @param target        if needed to send AR to an address, this is the target.
+ * @param winstonQty    amount of winston to send to the target, if needed.
+ */
+export async function simulateInteractWrite(
+  arweave: Arweave,
+  wallet: JWKInterface,
+  contractId: string,
+  input: any,
+  tags: { name: string; value: string }[] = [],
+  target: string = '',
+  winstonQty: string = '',
+): Promise<Transaction> {
+  const interactionTx = await createTx(arweave, wallet, contractId, input, tags, target, winstonQty);
+  return interactionTx;
+}
+
+/**
+ * This will load a contract to its latest state, and do a dry run of an interaction,
+ * without writing anything to the chain.
+ *
+ * @param arweave       an Arweave client instance
+ * @param wallet        a wallet private or public key
+ * @param contractId    the Transaction Id of the contract
+ * @param input         the interaction input.
+ * @param tags          an array of tags with name/value as objects.
+ * @param target        if needed to send AR to an address, this is the target.
+ * @param winstonQty    amount of winston to send to the target, if needed.
+ * @param myState       a locally-generated state variable
+ * @param fromParam     The from address of the transaction
+ * @param contractInfoParam The loaded contract
+ */
+export async function interactWriteDryRun(
+  arweave: Arweave,
+  wallet: JWKInterface | 'use_wallet',
+  contractId: string,
+  input: any,
+  tags: { name: string; value: string }[] = [],
+  target: string = '',
+  winstonQty: string = '',
+  myState?: any,
+  fromParam?: any,
+  contractInfoParam?: {
+    id: string;
+    contractSrc: string;
+    contractSrcTXID: string;
+    initState: string;
+    minFee: any;
+    contractTX: Transaction;
+    handler: ContractHandler;
+    swGlobal: SmartWeaveGlobal;
+  },
+): Promise<ContractInteractionResult> {
+  // tslint:disable-next-line: prefer-const
+  let { handler, swGlobal, contractSrcTXID } = contractInfoParam || (await loadContract(arweave, contractId));
+  const latestState = myState || (await readContract(arweave, contractId));
+  const from = fromParam || (await arweave.wallets.getAddress(wallet));
+
+  const settings = evalSettings(latestState);
+  const evolve: string = latestState.evolve || settings.get('evolve');
+  let canEvolve: boolean = latestState.canEvolve || settings.get('canEvolve');
+
+  // By default, contracts can evolve if there's not an explicit `false`.
+  if (canEvolve === undefined || canEvolve === null) {
+    canEvolve = true;
+  }
+
+  if (evolve && /[a-z0-9_-]{43}/i.test(evolve) && canEvolve) {
+    if (contractSrcTXID !== evolve) {
+      try {
+        const contractInfo = await loadContract(arweave, contractId, evolve);
+        swGlobal = contractInfo.swGlobal;
+        handler = contractInfo.handler;
+        contractSrcTXID = evolve;
+      } catch (e) {
+        const error: SmartWeaveError = new SmartWeaveError(SmartWeaveErrorType.CONTRACT_NOT_FOUND, {
+          message: `Contract having txId: ${contractId} not found`,
+          requestedTxId: contractId,
+        });
+        console.log(error);
+      }
+    }
+  }
+
+  const interaction: ContractInteraction = {
+    input,
+    caller: from,
+  };
+
+  const tx = await createTx(arweave, wallet, contractId, input, tags, target, winstonQty);
+
+  const ts = unpackTags(tx);
+
+  const currentBlock: BlockData = await arweave.blocks.getCurrent();
+
+  swGlobal._activeTx = createDummyTx(tx, from, ts, currentBlock);
+
+  return await execute(handler, interaction, latestState);
+}
+
+/**
+ * This will load a contract to its latest state, and do a dry run of an interaction,
+ * without writing anything to the chain.
+ *
+ * @param arweave       an Arweave client instance
+ * @param tx            a signed transaction
+ * @param contractId    the Transaction Id of the contract
+ * @param input         the interaction input.
+ * @param myState       a locally-generated state variable
+ * @param fromParam     The from address of the transaction
+ * @param contractInfoParam The loaded contract
+ */
+export async function interactWriteDryRunCustom(
+  arweave: Arweave,
+  tx: any,
+  contractId: string,
+  input: any,
+  myState: any,
+  fromParam: any = {},
+  contractInfoParam: any,
+): Promise<ContractInteractionResult> {
+  // tslint:disable-next-line: prefer-const
+  let { handler, swGlobal, contractSrcTXID } = contractInfoParam || (await loadContract(arweave, contractId));
+  const latestState = myState || (await readContract(arweave, contractId));
+  const from = fromParam;
+
+  const settings = evalSettings(latestState);
+  const evolve: string = latestState.evolve || settings.get('evolve');
+  let canEvolve: boolean = latestState.canEvolve || settings.get('canEvolve');
+
+  // By default, contracts can evolve if there's not an explicit `false`.
+  if (canEvolve === undefined || canEvolve === null) {
+    canEvolve = true;
+  }
+
+  if (evolve && /[a-z0-9_-]{43}/i.test(evolve) && canEvolve) {
+    if (contractSrcTXID !== evolve) {
+      try {
+        const contractInfo = await loadContract(arweave, contractId, evolve);
+        swGlobal = contractInfo.swGlobal;
+        handler = contractInfo.handler;
+        contractSrcTXID = evolve;
+      } catch (e) {
+        const error: SmartWeaveError = new SmartWeaveError(SmartWeaveErrorType.CONTRACT_NOT_FOUND, {
+          message: `Contract having txId: ${contractId} not found`,
+          requestedTxId: contractId,
+        });
+        console.log(error);
+      }
+    }
+  }
+
+  const interaction: ContractInteraction = {
+    input,
+    caller: from,
+  };
+
+  const ts = unpackTags(tx);
+
+  const currentBlock: BlockData = await arweave.blocks.getCurrent();
+
+  swGlobal._activeTx = createDummyTx(tx, from, ts, currentBlock);
+
+  return await execute(handler, interaction, latestState);
+}
+
+/**
+ * This will load a contract to its latest state, and execute a read interaction that
+ * does not change any state.
+ *
+ * @param arweave       an Arweave client instance
+ * @param wallet        a wallet private or public key
+ * @param contractId    the Transaction Id of the contract
+ * @param input         the interaction input.
+ * @param tags          an array of tags with name/value as objects.
+ * @param target        if needed to send AR to an address, this is the target.
+ * @param winstonQty    amount of winston to send to the target, if needed.
+ */
+export async function interactRead(
+  arweave: Arweave,
+  wallet: JWKInterface | 'use_wallet' | undefined,
+  contractId: string,
+  input: any,
+  tags: { name: string; value: string }[] = [],
+  target: string = '',
+  winstonQty: string = '',
+): Promise<any> {
+  // tslint:disable-next-line: prefer-const
+  let { handler, swGlobal, contractSrcTXID } = await loadContract(arweave, contractId);
+  const latestState = await readContract(arweave, contractId);
+  const from = wallet ? await arweave.wallets.getAddress(wallet) : '';
+
+  const settings = evalSettings(latestState);
+  const evolve: string = latestState.evolve || settings.get('evolve');
+  let canEvolve: boolean = latestState.canEvolve || settings.get('canEvolve');
+
+  // By default, contracts can evolve if there's not an explicit `false`.
+  if (canEvolve === undefined || canEvolve === null) {
+    canEvolve = true;
+  }
+
+  if (evolve && /[a-z0-9_-]{43}/i.test(evolve) && canEvolve) {
+    if (contractSrcTXID !== evolve) {
+      try {
+        const contractInfo = await loadContract(arweave, contractId, evolve);
+        swGlobal = contractInfo.swGlobal;
+        handler = contractInfo.handler;
+        contractSrcTXID = evolve;
+      } catch (e) {
+        const error: SmartWeaveError = new SmartWeaveError(SmartWeaveErrorType.CONTRACT_NOT_FOUND, {
+          message: `Contract having txId: ${contractId} not found`,
+          requestedTxId: contractId,
+        });
+        console.log(error);
+      }
+    }
+  }
+
+  const interaction: ContractInteraction = {
+    input,
+    caller: from,
+  };
+
+  const tx = await createTx(arweave, wallet, contractId, input, tags, target, winstonQty);
+  const ts = unpackTags(tx);
+  const currentBlock: BlockData = await arweave.blocks.getCurrent();
+
+  swGlobal._activeTx = createDummyTx(tx, from, ts, currentBlock);
+
+  const result = await execute(handler, interaction, latestState);
+
+  return result.result;
+}
+
+async function createTx(
+  arweave: Arweave,
+  wallet: JWKInterface | 'use_wallet',
+  contractId: string,
+  input: any,
+  tags: { name: string; value: string }[],
+  target: string = '',
+  winstonQty: string = '0',
+  reward?: string,
+): Promise<Transaction> {
+  const options: Partial<CreateTransactionInterface> = {
+    data: Math.random().toString().slice(-4),
+    reward,
+  };
+
+  if (target && target.length) {
+    options.target = target.toString();
+    if (winstonQty && +winstonQty > 0) {
+      options.quantity = winstonQty.toString();
+    }
+  }
+
+  const interactionTx = await arweave.createTransaction(options, wallet);
+
+  if (!input) {
+    throw new Error(`Input should be a truthy value: ${JSON.stringify(input)}`);
+  }
+
+  if (tags && tags.length) {
+    for (const tag of tags) {
+      interactionTx.addTag(tag.name.toString(), tag.value.toString());
+    }
+  }
+  interactionTx.addTag('App-Name', 'SmartWeaveAction');
+  interactionTx.addTag('App-Version', '0.3.0');
+  interactionTx.addTag('Contract', contractId);
+  interactionTx.addTag('Input', JSON.stringify(input));
+
+  await arweave.transactions.sign(interactionTx, wallet);
+  return interactionTx;
+}
+
+function createDummyTx(tx: Transaction, from: string, tags: Record<string, string | string[]>, block: BlockData) {
+  return {
+    id: tx.id,
+    owner: {
+      address: from,
+    },
+    recipient: tx.target,
+    tags,
+    fee: {
+      winston: tx.reward,
+    },
+    quantity: {
+      winston: tx.quantity,
+    },
+    block: {
+      id: block.indep_hash,
+      height: block.height,
+      timestamp: block.timestamp,
+    },
+  };
+}
---
+++ src/utils.ts
@@ -0,0 +1,160 @@
+import Transaction from 'arweave/node/lib/transaction';
+import Arweave from 'arweave';
+import { GQLEdgeInterface } from './interfaces/gqlResult';
+
+interface UnformattedTag {
+  name: string;
+  value: string;
+}
+
+export function getTag(tx: Transaction, name: string) {
+  const tags = tx.get('tags') as any;
+
+  for (const tag of tags) {
+    // decoding tags can throw on invalid utf8 data.
+    try {
+      if (tag.get('name', { decode: true, string: true }) === name) {
+        return tag.get('value', { decode: true, string: true });
+      }
+      // tslint:disable-next-line: no-empty
+    } catch (e) {}
+  }
+
+  return false;
+}
+
+/**
+ * Unpacks string tags from a Tx and puts in a KV map
+ * Tags that appear multiple times will be converted to an
+ * array of string values, ordered as they appear in the tx.
+ *
+ * @param tx
+ */
+export function unpackTags(tx: Transaction): Record<string, string | string[]> {
+  const tags = tx.get('tags') as any;
+  const result: Record<string, string | string[]> = {};
+
+  for (const tag of tags) {
+    try {
+      const name = tag.get('name', { decode: true, string: true }) as string;
+      const value = tag.get('value', { decode: true, string: true }) as string;
+      if (!result.hasOwnProperty(name)) {
+        result[name] = value;
+        continue;
+      }
+      result[name] = [...result[name], value];
+    } catch (e) {
+      // ignore tags with invalid utf-8 strings in key or value.
+    }
+  }
+  return result;
+}
+
+export function formatTags(tags: UnformattedTag[]): Record<string, string | string[]> {
+  const result: Record<string, string | string[]> = {};
+
+  for (const tag of tags) {
+    const { name, value } = tag;
+    if (!result.hasOwnProperty(name)) {
+      result[name] = value;
+      continue;
+    }
+    result[name] = [...result[name], value];
+  }
+
+  return result;
+}
+
+export function arrayToHex(arr: Uint8Array) {
+  let str = '';
+  for (const a of arr) {
+    str += ('0' + a.toString(16)).slice(-2);
+  }
+  return str;
+}
+
+export function log(arweave?: Arweave, ...str: string[]) {
+  if (!arweave || !arweave.getConfig().api.logging) return;
+
+  typeof arweave.getConfig().api.logger === 'function' ? arweave.getConfig().api.logger(...str) : console.log(...str);
+}
+
+export function normalizeContractSource(contractSrc: string): string {
+  // Convert from ES Module format to something we can run inside a Function.
+  // Removes the `export` keyword and adds ;return handle to the end of the function.
+  // Additionally it removes 'IIFE' declarations
+  // (which may be generated when bundling multiple sources into one output file
+  // - eg. using esbuild's "IIFE" bundle format).
+  // We also assign the passed in SmartWeaveGlobal to SmartWeave, and declare
+  // the ContractError exception.
+  // We then use `new Function()` which we can call and get back the returned handle function
+  // which has access to the per-instance globals.
+
+  contractSrc = contractSrc
+    .replace(/export\s+async\s+function\s+handle/gmu, 'async function handle')
+    .replace(/export\s+function\s+handle/gmu, 'function handle')
+    .replace(/\(\s*\(\)\s*=>\s*{/g, '')
+    .replace(/\s*\(\s*function\s*\(\)\s*{/g, '')
+    .replace(/}\s*\)\s*\(\)\s*;/g, '');
+
+  return `
+    const [SmartWeave, BigNumber, clarity] = arguments;
+    clarity.SmartWeave = SmartWeave;
+    class ContractError extends Error { constructor(message) { super(message); this.name = \'ContractError\' } };
+    function ContractAssert(cond, message) { if (!cond) throw new ContractError(message) };
+    ${contractSrc};
+    return handle;
+  `;
+}
+
+/**
+ * Function that evaluates the `settings` key of the state and return a valid Map.
+ * @param state
+ * @returns {Map} settings as a map
+ */
+export function evalSettings(state: any): Map<string, any> {
+  // default  - empty
+  let settings = new Map<string, any>();
+  if (state.settings) {
+    // for Iterable format
+    if (isIterable(state.settings)) {
+      settings = new Map<string, any>(state.settings);
+      // for Object format
+    } else if (isObject(state.settings)) {
+      settings = new Map<string, any>(Object.entries(state.settings));
+    }
+  }
+
+  return settings;
+}
+
+/**
+ * Checks if a variable is iterable.
+ * @param obj variable to if is iterable
+ * @returns
+ */
+function isIterable(obj: unknown): boolean {
+  // checks for null and undefined
+  if (obj == null) {
+    return false;
+  }
+  return typeof obj[Symbol.iterator] === 'function';
+}
+
+/**
+ * Check wether the variable is an object.
+ * @param obj variable to check if it's an object
+ * @returns
+ */
+function isObject(obj: unknown): boolean {
+  return typeof obj === 'object' && obj !== null && !Array.isArray(obj);
+}
+
+/**
+ * Checks if edge has multiple tags with the name 'Contract', which means multiple interactions.
+ * @param gqlResult
+ * @returns {boolean}
+ */
+export function hasMultipleinteractions(gqlResult: GQLEdgeInterface) {
+  return gqlResult.node.tags.filter((tag) => tag.name === 'Contract').length > 1;
+}
